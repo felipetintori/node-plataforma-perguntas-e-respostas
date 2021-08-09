d6f88232ffa0b204bfe4446f92ab7b483be24530
fetin3 <7822551-FelipeTintori@users.noreply.gitlab.com>
2021-08-09 08:19:01 -0300

    Estruturas de Repetição (forEach)

--- index.js
+++ index.js
@@ -5,16 +5,23 @@ const app = express();
 app.set('view engine', 'ejs');
 
 //Rota principal da aplicação - vai renderizar a index dentro da pasta views
-app.get("/:nome/:lang",(req, res) => {
+app.get("/",(req, res) => {
     var nome = req.params.nome;
     var lang = req.params.lang;
     var exibirMsg = false;
+
+    var produtos = [
+        {nome: "Doritos", preco: 3.14},
+        {nome: "Coca cola", preco: 5},
+        {nome: "Leite", preco: 1.14}
+    ]
     res.render("index",{
         nome: nome,
         lang: lang,
         empresa: "Guia do Programador",
         inscritos: 8000,
-        msg: exibirMsg
+        msg: exibirMsg,
+        produtos: produtos
     });
 })
 
