0526ceff93ef01aec0ab9742fd105fce8367e563
fetin3 <7822551-FelipeTintori@users.noreply.gitlab.com>
2021-08-13 08:55:42 -0300

    Criando Formulario de Perguntas

--- index.js
+++ index.js
@@ -7,23 +7,11 @@ app.use(express.static('public'))
 
 //Rota principal da aplicação - vai renderizar a index dentro da pasta views
 app.get("/",(req, res) => {
-    var nome = req.params.nome;
-    var lang = req.params.lang;
-    var exibirMsg = false;
+    res.render("index");
+});
 
-    var produtos = [
-        {nome: "Doritos", preco: 3.14},
-        {nome: "Coca cola", preco: 5},
-        {nome: "Leite", preco: 1.14}
-    ]
-    res.render("index",{
-        nome: nome,
-        lang: lang,
-        empresa: "Guia do Programador",
-        inscritos: 8000,
-        msg: exibirMsg,
-        produtos: produtos
-    });
+app.get("/perguntar",(req, res) =>{
+    res.render("perguntar")
 })
 
 //porta localhost
