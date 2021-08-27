523edd7660c27db2dc5d31d11327850c9ddc1fdd
fetin3 <7822551-FelipeTintori@users.noreply.gitlab.com>
2021-08-27 08:37:18 -0300

    Capturando dados do Formulario com body-parser

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 const express = require("express")
 const app = express();
+const bodyParser = require("body-parser")
 
 //usar EJS como renderizador de HTML
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 //Rota principal da aplicação - vai renderizar a index dentro da pasta views
 app.get("/",(req, res) => {
     res.render("index");
 });
 
+
 app.get("/perguntar",(req, res) =>{
     res.render("perguntar")
 })
 
 app.post("/salvarpergunta",(req, res) => {
-    res.send("Formulário Recebido")
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao
+    res.send("Formulário Recebido: Titulo " + titulo + " " + "descrção " + descricao)
 })
 //porta localhost
 app.listen(8080, ()=>{
